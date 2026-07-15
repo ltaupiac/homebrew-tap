b0f341e7445c825acbad6c27b7abc29e0edc8094
Laurent TAUPIAC <ltaupiac@lfdj.com>
2026-07-15 10:51:02 +0200

    feat(double-commander): Add double-commander cask

---
+++ Casks/double-commander.rb
@@ -0,0 +1,23 @@
+cask "double-commander" do
+  arch arm: "aarch64", intel: "x86_64"
+
+  version "1.2.6"
+
+  sha256 arm: :no_check,
+         intel: :no_check
+
+  url "https://github.com/doublecmd/doublecmd/releases/download/v#{version}/doublecmd-#{version}.cocoa.#{arch}.dmg"
+
+  name "Double Commander"
+  desc "Cross-platform file manager"
+  homepage "https://doublecmd.sourceforge.io/"
+
+  livecheck do
+    url "https://github.com/doublecmd/doublecmd/releases"
+    strategy :github_latest
+  end
+
+  app "Double Commander.app"
+
+  auto_updates true
+end
